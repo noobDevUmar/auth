27e7f20e15f906908430310259a9e51ec6786633
noobDevUmar <m.umarqureshi091@gmail.com>
2023-11-02 10:08:21 -0700

    basic starter

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,23 +1,38 @@
 import UserModel from "../models/UserModel.js"
 import {ErrorHandling} from "../utils/CustomError.js"
-export const signUp  = async (req,res,next)=>{
-    const {username,password,email} = req.body
+import bcrypt from "bcrypt"
+let  salt = bcrypt.genSaltSync(10)
 
-    try {
-        // check if username already exists
-       let check=  await UserModel.find({username})
-       if(check.length>0) 
-       {
-         throw ErrorHandling(402,"User Already Exisists")
-       }
-       const user = await UserModel.create({ username,email,password })
-       res.json({
-        user
-       })
+export const signUp = async (req, res, next) => {
+  const { username, password, email } = req.body;
 
-    } catch (error) {
-      next(error)
-    }
+  try {
+    // check if username already exists
+    let check = await UserModel.find({ username });
+    let hashedpassword = bcrypt.hashSync(password, salt);
 
+    const user = await UserModel.create({
+      username,
+      email,
+      password: hashedpassword,
+    });
+    res.json({ success:"true", user});
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async(req,res,next)=>{
+   let { email,password }= req.body;
+
+   let validUser = await UserModel.findOne({email})
+   if(!validUser) return next(ErrorHandling(401,"User not Found"))
+
+   let validpassword = bcrypt.compareSync(password,validUser.password)
+   if(!validpassword) return next(ErrorHandling(401,"passord did not matched"))
+
+   res.json({
+      message:
+      "User Matched"
+   })
 }
-
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,11 @@
 import express from "express"
 const router = express.Router()
-import {signUp} from "../controllers/auth.controller.js"
+import {signUp,signIn} from "../controllers/auth.controller.js"
 
 
 router.post('/signup',signUp)
+router.post('/login',signIn)
+
 
 
 export default router
