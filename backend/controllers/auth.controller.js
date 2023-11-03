562162966248c1ae1bc7f12225876628f6aa31c0
noobDevUmar <m.umarqureshi091@gmail.com>
2023-11-03 03:38:58 -0700

    signindone

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -26,19 +26,21 @@ export const signUp = async (req, res, next) => {
 };
 
 export const signIn = async(req,res,next)=>{
-   let { username,password }= req.body;
+   let { email,password }= req.body;
 try {
   
-   let validUser = await UserModel.findOne({email:username})
+   let validUser = await UserModel.findOne({email})
    if(!validUser) return next(ErrorHandling(401,"User not Found"))
 
    let validpassword = bcrypt.compareSync(password,validUser.password)
    if(!validpassword) return next(ErrorHandling(401,"passord did not matched"))
+//  its not safe to send validUser array complete because there is a password field too so we get the password field out from that
+const {password:hashedpassword,...rest} = validUser._doc
+   
 
    // Generate a token 
-
-  const token= jwt.sign({username,username:validUser.username},"mysecretkey");
-  res.cookie('token',token).json(validUser).status(200)
+  const token= jwt.sign({id:validUser._id},process.env.secret);
+  res.cookie('token',token).json(rest).status(200)
 
 } catch (error) {
   next(error)
